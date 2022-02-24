8be9ec5e4ce2062bcc866df4d5dfbcb45e82a537
Ankur Jain <ankuriiitb@yahoo.com>
2022-02-23 20:35:51 -0500

    Add codebuild / codedeploy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,6 +3,8 @@ STACK_NAME=awsbootstrap
 REGION=us-east-1
 CLI_PROFILE=awsbootstrap
 EC2_INSTANCE_TYPE=t2.micro
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
@@ -13,6 +15,7 @@ aws cloudformation deploy \
  --no-fail-on-empty-changeset \
  --capabilities CAPABILITY_NAMED_IAM \
  --parameter-overrides \
+   CodePipelineBucket=$CODEPIPELINE_BUCKET
    EC2InstanceType=$EC2_INSTANCE_TYPE
 
 # If the deploy succeeded, show the DNS name of the created instance
